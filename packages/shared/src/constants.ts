4d9d2be6b68acc80a6ea17bc9c41bbc1e6bee312
Obradović Jovica <jolpazy@gmail.com>
2025-11-23 12:10:27 +0000

    add constants

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,32 @@
+export const title = "Search github repos:";
+
+export const colors = {
+  reactGrey: "#1e1e24",
+  reactBlue: "#61dafb",
+  white: "#ffffff",
+  black: "#000000",
+  reactLightGrey: "#333A45",
+};
+
+export const fontSizes = {
+  xl: 48,
+  lg: 24,
+  md: 16,
+  sm: 14,
+};
+
+export const space = {
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 24,
+  xxl: 32,
+};
+
+export const radii = {
+  pill: 999,
+  md: 8,
+};
+
+export const DEBOUNCE_MS = 2000;
